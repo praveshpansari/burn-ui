ece112434fbf1b25e0fec054bfd9076ed8779641
Pravesh Pansari <praveshpansari@gmail.com>
2022-10-28 17:46:14 +0530

    fix prettier

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,11 +1,9 @@
-import { Component } from "solid-js";
+import { Component } from 'solid-js';
 
 const Home: Component = () => {
   return (
     <section class="bg-gray-50 dark:bg-primary-900">
-      <div class="flex flex-col items-center justify-center px-6 mx-auto py-16">
-        Home
-      </div>
+      <div class="flex flex-col items-center justify-center px-6 mx-auto py-16">Home</div>
     </section>
   );
 };
--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,19 +1,17 @@
-import { A } from "@solidjs/router";
-import { Component } from "solid-js";
-import flame from "../../assets/flame.png";
+import { A } from '@solidjs/router';
+import { Component } from 'solid-js';
+import flame from '../../assets/flame.png';
 
 const Header: Component = () => {
   let linkClass =
-    "block py-2 pr-4 pl-3 text-primary-600 border-b border-primary-100 hover:bg-primary-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-primary-400 lg:dark:hover:text-white dark:hover:bg-primary-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-primary-700";
+    'block py-2 pr-4 pl-3 text-primary-600 border-b border-primary-100 hover:bg-primary-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-primary-400 lg:dark:hover:text-white dark:hover:bg-primary-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-primary-700';
   return (
     <header>
       <nav class="bg-white border-primary-200 px-4 lg:px-6 py-2.5 dark:bg-primary-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a href="/" class="flex items-center">
             <img src={flame} class="mr-3 h-6 sm:h-9" alt="Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Cashburn
-            </span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Cashburn</span>
           </a>
           <div class="flex items-center lg:order-2">
             <a
@@ -36,24 +34,14 @@ const Header: Component = () => {
               aria-expanded="false"
             >
               <span class="sr-only">Open main menu</span>
-              <svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path
                   fill-rule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <svg
-                class="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path
                   fill-rule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -62,10 +50,7 @@ const Header: Component = () => {
               </svg>
             </button>
           </div>
-          <div
-            class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
-          >
+          <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
             <ul class="flex flex-col mt-4 font-medium text-sm tracking-wide lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <A
